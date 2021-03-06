10b065442d0495c8b91f1b2fb54e74ec8b8899fb
drewenia <egedata@gmail.com>
2021-03-06 18:10:57 +0300

    Pointer'lar ile ilgili ilk ornek - Ampersand kullanimi ve Pointer.md dosyasinin create edilmesi

---
+++ first-app/Pointers.md
@@ -0,0 +1,14 @@
+**Pointers**
+
+accountNumber degiskeninde 300 degerini sakla,
+
+dolayisiyla burada iki sey alabilirim; 
+
+degiskenin adresi ve degiskenin tuttugu deger
+
+accountNumberPointer degiskenini *int ile tanitiyorum;
+
+accountNumberPointer i &accountNumber degiskenine esitlersen accountNumber'a ait adres degerini alabilirim (&ampersand)
+
+Eger accountNumberPointer'i *accountNumberPointer seklinde ekrana basarsam ekranda 300 degerini goruyorum
+
--- first-app/main.go
+++ first-app/main.go
@@ -3,27 +3,15 @@ package main
 import "fmt"
 
 type Account struct{
-	/*
-		Class'lar yerine go'da bu sekilde type'lar tanimlanabiliyor
-		Icerisinde method ya da baska herhangi bir yapi tanimlanamiyor
-	*/
 	AccountNumber int
 	FirstName string
 	LastName string
 }
 
-//GetFullName fonksiyonunu Account tipine tanimla
-func (account *Account) GetFullName() string{
-	return account.FirstName + " " + account.LastName
-}
-
-func (account *Account) ChangeFirstName(firstName string) {
-	account.FirstName = firstName
-}
-
 func main(){
-	//:= kullanimi variable'i hem create eder hemde initialize eder
-	account:=Account{1,"Okyanus","Kuce"}
-	account.ChangeFirstName("Cihangir")
-	fmt.Println(account.GetFullName())
+	var accountNumber int = 300
+	var accountNumberPointer *int = &accountNumber //& ampersand isareti degeri degil adresi verir
+
+	fmt.Println(accountNumberPointer)
+	fmt.Println(*accountNumberPointer)
 }
